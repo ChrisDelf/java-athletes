968dad8bc61386b1ea84b0d58701581f44642eee
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-09-05 15:23:35 -0400

    initial commit

---
+++ athlete/src/com/lambdaschool/solution/MyApplication.java
@@ -0,0 +1,19 @@
+package com.lambdaschool.solution;
+
+public class MyApplication  implements Processor {    // Implementing the processor, we need to display the athlete
+   //constructor injection
+    private AthleteCreation athlete;
+
+
+    public MyApplication(AthleteCreation athlete) { // athlete is the argument that is passed in to the function that creates the athlete.
+
+        this.athlete = athlete;
+    }
+
+    @Override
+    public void displayAthlete() {
+        System.out.println("******************");
+        athlete.displayAthlete();
+
+    }
+}
