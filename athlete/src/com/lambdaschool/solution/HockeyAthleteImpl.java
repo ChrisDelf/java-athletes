a97730b14db4ec4498e7ddf151c21b5a1ba9a86b
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-09-05 16:01:02 -0400

    Added injectors and implmeneters and nothing broke

---
+++ athlete/src/com/lambdaschool/solution/HockeyAthleteImpl.java
@@ -0,0 +1,8 @@
+package com.lambdaschool.solution;
+
+public class HockeyAthleteImpl implements AthleteCreation{
+    @Override
+    public void displayAthlete() {
+        System.out.println("Track Runner");
+    }
+}
